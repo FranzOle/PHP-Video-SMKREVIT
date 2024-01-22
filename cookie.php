6cad33f618612fb773753a22f02e2c0d7a77a8df
Lionel jevon <lioneljevon1212@gmail.com>
2024-01-22 13:46:08 +0700

    Video 15
    Cookie php

---
+++ cookie.php
@@ -0,0 +1,16 @@
+<?php 
+
+$cookie_name = "polisi";
+$cookie_value = "ferdi sambo";
+setcookie($cookie_name, $cookie_value);
+
+$cookie_value = "yosua";
+setcookie($cookie_name, $cookie_value);
+
+echo $_COOKIE[$cookie_name];
+
+echo '<br>';
+
+setcookie("user", "", time() - 3600);
+var_dump($_COOKIE);
+?>
