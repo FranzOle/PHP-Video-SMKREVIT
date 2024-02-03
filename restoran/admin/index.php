b68c5d75b76d34608e67f45af8391bc388fff8eb
Lionel jevon <lioneljevon1212@gmail.com>
2024-02-03 23:26:55 +0700

    Video 26
    Mengatur Menu

--- restoran/admin/index.php
+++ restoran/admin/index.php
@@ -1,3 +1,9 @@
+<?php 
+
+    require_once "../dbcontrol.php";
+    $db = new DB;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,15 +27,26 @@
             <div class="col-md-3">
                 <!-- <h2>Admin Page</h2> -->
                 <ul class="nav flex-column">
-                    <li class="nav-item"><a class="nav-link" href="#">Kategori</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Menu</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Pelanggan</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Order</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Detail Order</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">User</a></li>
+                    <li class="nav-item"><a class="nav-link" href="?f=kategori&m=select">Kategori</a></li>
+                    <li class="nav-item"><a class="nav-link" href="?f=menu&m=select">Menu</a></li>
+                    <li class="nav-item"><a class="nav-link" href="?f=pelanggan&m=select">Pelanggan</a></li>
+                    <li class="nav-item"><a class="nav-link" href="?f=order&m=select">Order</a></li>
+                    <li class="nav-item"><a class="nav-link" href="?f=orderdetail&m=select">Detail Order</a></li>
+                    <li class="nav-item"><a class="nav-link" href="?f=user&m=select">User</a></li>
                 </ul>
             </div>
-            <div class="col-md-9"></div>
+            <div class="col-md-9">
+                <!-- <h1>Tes</h1> -->
+                <?php
+                if (isset($_GET['f']) && isset($_GET['m'])) {
+                        $f = $_GET['f'];
+                        $m = $_GET['m'];
+                        $file = '../'.$f.'/'.$m.'.php';
+
+                        require_once $file;
+                    }
+                ?>
+            </div>
         </div>
 
         <div class="row">
