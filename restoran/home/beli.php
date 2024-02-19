34398b1e93b59d012566b640d43e360c1c4111d4
FranzOle <115760743+FranzOle@users.noreply.github.com>
2024-02-19 16:56:35 +0700

    Video 43
    Shopping Cart

---
+++ restoran/home/beli.php
@@ -0,0 +1,52 @@
+<h1>Keranjang Belanja</h1>
+
+<?php
+if (!isset($_SESSION['pelanggan'])) {
+    header("location:?f=home&m=login");
+}
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    if (isset($_SESSION['_' . $id])) {
+        $_SESSION['_' . $id]++;
+    } else {
+        $_SESSION['_' . $id] = 1;
+    }
+
+    echo '
+    <table class="table table-bordered w-70 mt-4">
+    <tr>
+        <th>No</th>
+        <th>Harga</th>
+        <th>Jumlah</th>
+        <th>Total</th>
+    </tr>
+    <tbody>
+    
+    ';
+
+    foreach ($_SESSION as $key => $value) {
+        if ($key != 'pelanggan' && $key != 'idpelanggan') {
+            $id = substr($key, 1);
+
+            $sql = "SELECT * FROM tblmenu WHERE idmenu=$id";
+            $row = $db->getALL($sql);
+
+            foreach ($row as $r) {
+                echo '<tr>';
+                echo '<td>' . $r['menu'] . '</td>';
+                echo '<td>' . $r['harga'] . '</td>';
+                echo '<td>' . $value . '</td>';
+                echo '<td>' . $r['harga'] * $value . '</td>';
+                echo '</tr>';
+            }
+        }
+    }
+
+    echo '
+    </tbody>
+    </table>';
+
+}
+?>
