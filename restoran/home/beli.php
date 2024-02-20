ab6aaf5477856f82cbd998285b944a3936c35bbf
FranzOle <115760743+FranzOle@users.noreply.github.com>
2024-02-20 20:35:13 +0700

    Video 44
    Menghapus isi  Shopping Cart

--- restoran/home/beli.php
+++ restoran/home/beli.php
@@ -1,37 +1,55 @@
 <h1>Keranjang Belanja</h1>
 
 <?php
-if (!isset($_SESSION['pelanggan'])) {
-    header("location:?f=home&m=login");
+
+if (isset($_GET['hapus'])) {
+    $id = $_GET['hapus'];
+    unset($_SESSION['_' . $id]);
 }
 
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
+if (!isset($_SESSION['pelanggan'])) {
+    header("location:?f=home&m=login");
+} else {
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        isi($id);
+        header("location:?f=home&m=beli");
+    } else {
+        Keranjang(); 
+    }
+}
 
+function isi($id) {
     if (isset($_SESSION['_' . $id])) {
         $_SESSION['_' . $id]++;
     } else {
         $_SESSION['_' . $id] = 1;
     }
+}
 
+function Keranjang() {
+    global $db;
     echo '
-    <table class="table table-bordered w-70 mt-4">
+
+    <table class="table table-bordered w-50">
+    
     <tr>
         <th>No</th>
         <th>Harga</th>
         <th>Jumlah</th>
         <th>Total</th>
+        <th>Hapus</th>
     </tr>
     <tbody>
-    
     ';
 
     foreach ($_SESSION as $key => $value) {
-        if ($key != 'pelanggan' && $key != 'idpelanggan') {
+        if ($key <> 'pelanggan' && $key <> 'idpelanggan') {
             $id = substr($key, 1);
 
             $sql = "SELECT * FROM tblmenu WHERE idmenu=$id";
-            $row = $db->getALL($sql);
+
+            $row = $db -> getALL($sql);
 
             foreach ($row as $r) {
                 echo '<tr>';
@@ -39,14 +57,13 @@ if (isset($_GET['id'])) {
                 echo '<td>' . $r['harga'] . '</td>';
                 echo '<td>' . $value . '</td>';
                 echo '<td>' . $r['harga'] * $value . '</td>';
+                echo '<td> <a href="?f=home&m=beli&hapus=' . $r['idmenu'] . '">Hapus</a> </td>';
                 echo '</tr>';
             }
         }
     }
-
     echo '
     </tbody>
     </table>';
-
 }
 ?>
